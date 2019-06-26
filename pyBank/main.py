40966bc6b4478443c76884ede4709935505971b1
scobbyy2k3 <49992237+scobbyy2k3@users.noreply.github.com>
2019-06-26 13:44:48 -0400

    python challenge homework

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,17 +1,19 @@
 import os
 import csv
 
-filename = input("Please enter the filename for the data you would like to review: ")
+#path for file
+filepath = os.path.join("C:\\Users\\HADEORLAH\\Desktop\\python-challenge\\pyBank\\Resources\\budget_data.csv")
+
+
 month_count = 0
 total_revenue = 0
-this_month_revenue = 0
-last_month_revenue = 0
+currentmonth_revenue = 0
+previous_revenue = 0
 revenue_change = 0
 revenue_changes = []
 months = []
 
 # open csv file
-filepath = "Resources/budget_data.csv"
 with open(filepath,'r', newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
@@ -19,24 +21,39 @@ with open(filepath,'r', newline="") as csvfile:
 
     # gather monthly changes in revenue
     for row in csvreader:
-        month_count = month_count + 1
-        months.append(row[0])
-        this_month_revenue = int(row[1])
-        total_revenue = total_revenue + this_month_revenue
-        if month_count > 1:
-            revenue_change = this_month_revenue - last_month_revenue
-            revenue_changes.append(revenue_change)
-        last_month_revenue = this_month_revenue
 
-# analyze the month by month results
+# number of months in dataset
+        month_count = month_count + 1
+
+#revenue change by month
+        months.append(row[0])
+        currentmonth_revenue = int(row[1])
+        total_revenue = total_revenue + currentmonth_revenue
+
+        if month_count > 1:
+            revenue_change = currentmonth_revenue - previous_revenue
+            revenue_changes.append(revenue_change)
+
+
+        previous_revenue = currentmonth_revenue
+
+#  month to month calcualtion of revenue change
 sum_rev_changes = sum(revenue_changes)
+
 average_change = sum_rev_changes / (month_count - 1)
+
 max_change = max(revenue_changes)
+
 min_change = min(revenue_changes)
+
 max_month_index = revenue_changes.index(max_change)
+
 min_month_index = revenue_changes.index(min_change)
+
 max_month = months[max_month_index]
+
 min_month = months[min_month_index]
+
 
 # print summary to user
 print("Financial Analysis")
@@ -44,17 +61,20 @@ print("----------------------------------------")
 print(f"Total Months: {month_count}")
 print(f"Total Revenue: ${total_revenue}")
 print(f"Average Revenue Change: ${average_change}")
-print(f"Greatest Increase in Revenue: {max_month} (${max_change})")
-print(f"Greatest Decrease in Revenue: {min_month} (${min_change})")
+print(f"Greatest Increase in Revenue: {max_month} (${max_change}) ")
+print(f"Greatest Decrease in Revenue: {min_month} (${min_change}) ")
 
-# save summary to txt
-save_file = filename.strip(".csv") + "_results.txt"
-filepath = os.path.join(".", save_file)
-with open(filepath,'w') as text:
-    text.write("Financial Analysis" + "\n")
-    text.write("----------------------------------------" + "\n")
-    text.write(f"Total Months: {month_count}" + "\n")
-    text.write(f"Total Revenue: ${total_revenue}" + "\n")
-    text.write(f"Average Revenue Change: ${average_change}" + "\n")
-    text.write(f"Greatest Increase in Revenue: {max_month} (${max_change})" + "\n")
-    text.write(f"Greatest Decrease in Revenue: {min_month} (${min_change})" + "\n") 
+ # Name white file
+output_file = filepath [0:-4]
+
+write_budget_dataCSV = f"{output_file}_pybank_results.txt"
+
+       # Open write file
+text = open(write_budget_dataCSV, mode = 'w')
+text.write("Financial Analysis" + "\n")
+text.write("----------------------------------------" + "\n")
+text.write(f"Total Months: {month_count}" + "\n")
+text.write(f"Total Revenue: ${total_revenue}" + "\n")
+text.write(f"Average Revenue Change: ${average_change}" + "\n")
+text.write(f"Greatest Increase in Revenue: {max_month} (${max_change})" + "\n")
+text.write(f"Greatest Decrease in Revenue: {min_month} (${min_change})" + "\n") 
